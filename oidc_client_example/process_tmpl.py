8817a84bef2beecb79d35362601f2335c8f3109b
kelvin <kelvin.glass@fu-berlin.de>
2017-08-13 18:23:08 +0200

    Change OIDC client library and use popup authentication
    * Add redirect_url and authority_url to template
     --> use env variables REDIRECT_URL, AUTHORITY_URL
    * Use oidc client of https://github.com/IdentityModel/oidc-client-js
      instead of https://nat.sakimura.org/2014/12/10/making-a-javascript-openid-connect-client/
      since the second has neither a repository, license nor documentation
    * Use popup to authenticate

--- oidc_client_example/process_tmpl.py
+++ oidc_client_example/process_tmpl.py
@@ -3,17 +3,27 @@ import os
 import sys
 
 client_id_key = 'CLIENT_ID'
+authority_url_key = 'AUTHORITY_URL'
+redirect_url_key = 'REDIRECT_URL'
 
-# Check whether client id is provided
-if not client_id_key in os.environ:
-    print 'ERROR: Environment variable CLIENT_ID has to be provided'
-    sys.exit(-1)
+def getEnv(key):
+    if not key in os.environ:
+        print "ERROR: Environment variable " + key + " has to be provided"
+        sys.exit(-1)
+    return os.environ[key]
+    
 
-client_id = os.environ[client_id_key]
+client_id = getEnv(client_id_key)
+authority_url = getEnv(authority_url_key)
+redirect_url = getEnv(redirect_url_key)
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 jinja_env = Environment(loader = FileSystemLoader(cwd))
 
 html_file = open (cwd + "/index.html", 'w')
-html_file.write (jinja_env.get_template('index_tmp.html').render(client_id = client_id))
+html_file.write (jinja_env.get_template('index_tmp.html').render(
+    client_id = client_id,
+    redirect_url = redirect_url,
+    authority_url = authority_url
+))
 html_file.close()
