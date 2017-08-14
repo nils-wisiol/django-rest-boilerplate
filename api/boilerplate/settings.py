39ca80137970f3beb940dc9833403afd53cb12d2
Niels Groth <niels.groth@grothbande.de>
2017-08-14 15:41:22 +0200

    added templates and configured urls

--- api/boilerplate/settings.py
+++ api/boilerplate/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'oidc_provider',
+    'boilerplate',
 ]
 
 MIDDLEWARE = [
--- api/boilerplate/urls.py
+++ api/boilerplate/urls.py
@@ -19,7 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework import routers
 from . import views
-
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -28,4 +29,10 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api/admin/', admin.site.urls),
+
+    url(r'^api/openid/$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^api/openid/accounts/login/$', auth_views.login, { 'template_name': 'login.html' }, name='login'),
+    url(r'^api/openid/accounts/logout/$', auth_views.logout, { 'next_page': '/' }, name='logout'),
+
+    url(r'^api/openid/', include('oidc_provider.urls', namespace='oidc_provider')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
